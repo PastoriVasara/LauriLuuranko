9d7661eac59a14ab91b3bfca4c8552380913d051
Jussi <jussi.p.rantala@tuni.fi>
2023-06-21 22:41:51 +0300

    audioplaying

--- soundev.py
+++ soundev.py
@@ -6,3 +6,10 @@ for device in devices:
         print('Device name:', device['name'])
         print('Maximum input channels:', device['max_input_channels'])
         print()
+
+devices = sd.query_devices()
+for device in devices:
+    if device['max_input_channels'] > 0:
+        print('Device name:', device['name'])
+        print('Default samplerate:', device['default_samplerate'])
+        print()
