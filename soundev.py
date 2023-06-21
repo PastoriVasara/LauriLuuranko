1df09466dfa8154d6781f66e8b7a37941a8dab91
Jussi <jussi.p.rantala@tuni.fi>
2023-06-21 22:38:46 +0300

    sd

---
+++ soundev.py
@@ -0,0 +1,8 @@
+import sounddevice as sd
+
+devices = sd.query_devices()
+for device in devices:
+    if device['max_input_channels'] > 0:
+        print('Device name:', device['name'])
+        print('Maximum input channels:', device['max_input_channels'])
+        print()
